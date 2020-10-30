eace2e822c6cf859cac920ef3e79130a9222d85f
Quentin Dunand <quentin.dunand@non.se.com>
2020-10-30 15:47:45 +0100

    Enregistrer et restaurer automatiquement les données du formulaire
    Reprise du travail fait par @theblackhole
    - Enregistrement chiffré des données du formulaire dans le localStorage à la génération du pdf
    - Récupération des données chiffrées à l'ouverture du formulaire avec autocompletion des champs (profile et choix raison)
    - Autocompletion de la date de sortie (avec date et heure actuelle)
    - Ajout d'une checkbox pour choisir ou non d'enregister les données et d'un lien pour effacer les données enregistrées :
      - si la checkbox est décochée, les données sont effacées du localStorage mais pas du formulaire
      - si le lien pour effacer les données est cliqué, les données sont effacées du localStorage et du formulaire

--- src/js/form-util.js
+++ src/js/form-util.js
@@ -2,6 +2,11 @@ import { $, $$, downloadBlob } from './dom-utils'
 import { addSlash, getFormattedDate } from './util'
 import pdfBase from '../certificate.pdf'
 import { generatePdf } from './pdf-util'
+import SecureLS from 'secure-ls'
+
+const secureLS = new SecureLS({ encodingType: 'aes' })
+const clearDataSnackbar = $('#snackbar-cleardata')
+const storeDataInput = $('#field-storedata')
 
 const conditions = {
   '#field-firstname': {
@@ -53,6 +58,45 @@ function validateAriaFields () {
     .includes(true)
 }
 
+function updateSecureLS (formInputs, reasonInputs) {
+  if (wantDataToBeStored() === true) {
+    secureLS.set('profile', getProfile(formInputs))
+    secureLS.set('reason', getReasonsObject(reasonInputs))
+  } else {
+    clearSecureLS()
+  }
+}
+
+function clearSecureLS () {
+  secureLS.clear()
+}
+
+function clearForm () {
+  const formProfile = $('#form-profile')
+  formProfile.reset()
+}
+
+function setCurrentDate (releaseDateInput, releaseTimeInput) {
+  const currentDate = new Date()
+
+  releaseDateInput.value = getFormattedDate(currentDate)
+  releaseTimeInput.value = currentDate.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })
+}
+
+function showSnackbar (snackbarToShow, showDuration = 6000) {
+  snackbarToShow.classList.remove('d-none')
+  setTimeout(() => snackbarToShow.classList.add('show'), 100)
+
+  setTimeout(function () {
+    snackbarToShow.classList.remove('show')
+    setTimeout(() => snackbarToShow.classList.add('d-none'), 500)
+  }, showDuration)
+}
+
+export function wantDataToBeStored () {
+  return storeDataInput.checked
+}
+
 export function setReleaseDateTime (releaseDateInput) {
   const loadedDate = new Date()
   releaseDateInput.value = getFormattedDate(loadedDate)
@@ -78,8 +122,40 @@ export function getReasons (reasonInputs) {
   return reasons
 }
 
-export function prepareInputs (formInputs, reasonInputs, reasonFieldset, reasonAlert, snackbar) {
+export function getReasonsObject (reasonInputs) {
+  const reasons = reasonInputs
+    .filter((reason) => reason.checked)
+    .reduce(function (map, reason) {
+      map[reason.value] = reason.checked
+      return map
+    }, {})
+  return reasons
+}
+
+export function prepareInputs (formInputs, reasonInputs, reasonFieldset, reasonAlert, snackbar, releaseDateInput, releaseTimeInput) {
+  const lsProfile = secureLS.get('profile')
+  const lsReason = secureLS.get('reason')
+  const currentDate = new Date()
+  const formattedDate = getFormattedDate(currentDate)
+  const formattedTime = currentDate.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })
+
+  // Continue to store data if already stored
+  storeDataInput.checked = lsReason || lsProfile
+
   formInputs.forEach((input) => {
+    switch (input.name) {
+      case 'datesortie':
+        input.value = formattedDate
+        break
+      case 'heuresortie':
+        input.value = formattedTime
+        break
+      case 'field-reason':
+        if (lsReason) input.checked = lsReason[input.value]
+        break
+      default:
+        if (lsProfile) input.value = lsProfile[input.name]
+    }
     const exempleElt = input.parentNode.parentNode.querySelector('.exemple')
     const validitySpan = input.parentNode.parentNode.querySelector('.validity')
     if (input.placeholder && exempleElt) {
@@ -111,6 +187,17 @@ export function prepareInputs (formInputs, reasonInputs, reasonFieldset, reasonA
     })
   })
 
+  $('#formgroup-storedata').addEventListener('click', () => {
+    (storeDataInput.checked) ? storeDataInput.checked = false : storeDataInput.checked = true
+  })
+
+  $('#cleardata').addEventListener('click', () => {
+    clearSecureLS()
+    clearForm()
+    setCurrentDate(releaseDateInput, releaseTimeInput)
+    showSnackbar(clearDataSnackbar, 1200)
+  })
+
   $('#generate-btn').addEventListener('click', async (event) => {
     event.preventDefault()
 
@@ -127,7 +214,7 @@ export function prepareInputs (formInputs, reasonInputs, reasonFieldset, reasonA
       return
     }
 
-    console.log(getProfile(formInputs), reasons)
+    updateSecureLS(formInputs, reasonInputs)
 
     const pdfBlob = await generatePdf(getProfile(formInputs), reasons, pdfBase)
 
@@ -156,6 +243,7 @@ export function prepareForm () {
   const reasonFieldset = $('#reason-fieldset')
   const reasonAlert = reasonFieldset.querySelector('.msg-alert')
   const releaseDateInput = $('#field-datesortie')
+  const releaseTimeInput = $('#field-heuresortie')
   setReleaseDateTime(releaseDateInput)
-  prepareInputs(formInputs, reasonInputs, reasonFieldset, reasonAlert, snackbar)
+  prepareInputs(formInputs, reasonInputs, reasonFieldset, reasonAlert, snackbar, releaseDateInput, releaseTimeInput)
 }
