3c32866bfcd8211a2224cd2f5044030b05cb501a
NICOLAPTOP <nicopowa@gmail.com>
2020-11-05 18:16:55 +0100

    update main code update form

--- patch/entry.js
+++ patch/entry.js
@@ -2,20 +2,22 @@ import {generatePdf} from "../src/js/pdf-util.js"
 
 window.addEventListener("load", async () => {
 
-	document.body.innerHTML += "initialisation du cluster ...<br/>";
+	document.body.innerHTML += "covid rapide<br/>initialisation du cluster<br/>";
 
-	let infos = Object.fromEntries(new URLSearchParams(location.search)), 
-		date = moment().subtract(infos.minutes || 10, "minutes");
+	let infos = Object.fromEntries(new URLSearchParams(location.search));
+
+	if(Object.keys(infos).length !== 9) return document.body.innerHTML += "info manquantes<br/>";
 	
-	document.body.innerHTML += "création de l'attestation...<br/>";
+	document.body.innerHTML += "édition de l'attestation<br/>";
 
-	let blob = new Blob([await generatePdf({...infos, datesortie: date.format("DD[/]MM[/]YYYY"), heuresortie: date.format("HH[:]mm")}, infos.motif || "achats", "src/certificate.pdf", date)], {type: "application/pdf"}), 
+	let date = moment().subtract(infos.minutes || 10, "minutes"), 
+		blob = new Blob([await generatePdf({...infos, datesortie: date.format("DD[/]MM[/]YYYY"), heuresortie: date.format("HH[:]mm")}, infos.motif || "achats", "src/certificate.pdf", date)], {type: "application/pdf"}), 
 		link = document.createElement("a");
-
-	document.body.innerHTML += "téléchargement ...";
 
 	link.href = window.URL.createObjectURL(blob);
 	link.download = "attestation-" + date.format("YYYY[-]MM[-]DD[_]HH[-]mm") + ".pdf";
 	link.click();
+
+	document.body.innerHTML += "ok<br/>";
 	
 });
