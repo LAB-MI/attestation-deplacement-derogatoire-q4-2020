a817a5a7fe3d61391b2e4d23312c718d728e642c
Louis Rabiet <louis@proprioo.fr>
2020-11-26 22:43:49 +0100

    cleanup reason in form.js

--- src/js/form.js
+++ src/js/form.js
@@ -74,59 +74,6 @@ const createFormGroup = ({
   return formGroup
 }
 
-const createReasonField = (reasonData) => {
-  const formReasonAttrs = { className: 'form-checkbox align-items-center' }
-  const formReason = createElement('div', formReasonAttrs)
-  const appendToReason = appendTo(formReason)
-
-  const id = `checkbox-${reasonData.code}`
-  const inputReasonAttrs = {
-    className: 'form-check-input',
-    type: 'checkbox',
-    id,
-    name: 'field-reason',
-    value: reasonData.code,
-  }
-  const inputReason = createElement('input', inputReasonAttrs)
-
-  const labelAttrs = { innerHTML: reasonData.label, className: 'form-checkbox-label', for: id }
-  const label = createElement('label', labelAttrs)
-
-  appendToReason([inputReason, label])
-  return formReason
-}
-
-const createReasonFieldset = (reasonsData) => {
-  const fieldsetAttrs = {
-    id: 'reason-fieldset',
-    className: 'fieldset',
-  }
-
-  const fieldset = createElement('fieldset', fieldsetAttrs)
-  const appendToFieldset = appendTo(fieldset)
-
-  const legendAttrs = {
-    className: 'legend titre-3',
-    innerHTML: 'Choisissez un motif de déplacement',
-  }
-  const legend = createElement('legend', legendAttrs)
-
-  const textAlertAttrs = { className: 'msg-alert hidden', innerHTML: 'Veuillez choisir un motif' }
-  const textAlert = createElement('p', textAlertAttrs)
-
-  const textSubscribeReasonAttrs = {
-    innerHTML: 'certifie que mon déplacement est lié au motif suivant (cocher la case) autorisé par le décret n°2020-1310 du 29 octobre 2020 prescrivant les mesures générales nécessaires pour faire face à l\'épidémie de Covid19 dans le cadre de l\'état d\'urgence sanitaire  <a class="footnote" href="#footnote1">[1]</a>&nbsp;:',
-  }
-
-  const textSubscribeReason = createElement('p', textSubscribeReasonAttrs)
-
-  const reasonsFields = reasonsData.items.map(createReasonField)
-
-  appendToFieldset([legend, textAlert, textSubscribeReason, ...reasonsFields])
-  // Créer un form-checkbox par motif
-  return fieldset
-}
-
 export function createForm () {
   const form = $('#form-profile')
   // Évite de recréer le formulaire s'il est déjà créé par react-snap (ou un autre outil de prerender)
@@ -150,11 +97,5 @@ export function createForm () {
 
       return formGroup
     })
-
-  const reasonsData = formData
-    .flat(1)
-    .find(field => field.key === 'reason')
-
-  const reasonFieldset = createReasonFieldset(reasonsData)
   appendToForm([...createTitle(), ...formFirstPart])
 }
