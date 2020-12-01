e7efba9e3a4e5e075bb45435eea46d2e28e98e10
Joel Pagniez <joel.pagniez@gmail.com>
2020-12-01 15:01:54 +0100

    Adaptation de l'attestation aux nouvelles dispositions en vigueur à partir du 28/11
    - Modification du formulaire
    - Modification du pdf (certificate.pdf)
    - Repositionnement des textes ajoutés
    - Ajout des liens application TousAntiCovid

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,12 @@
 - Modification du pdf (certificate.pdf)
 - Repositionnement des textes ajoutés
 - Accessibilité : ajout d'un texte (en blanc) pour indiquer aux lecteurs de pdf audio la présence d'un QR-code dans le document généré
+
+## [1.1.1]
+
+- Modification du pdf (certificate.pdf)
+- Repositionnement des textes ajoutés
+
+## [1.1.2]
+
+- Ajout des liens application TousAntiCovid
--- src/js/pdf-util.js
+++ src/js/pdf-util.js
@@ -2,15 +2,15 @@ import { generateQR } from './util'
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib'
 
 const ys = {
-  travail: 488,
-  achats: 417,
-  sante: 347,
-  famille: 325,
-  handicap: 291,
-  sport_animaux: 269,
-  convocation: 199,
-  missions: 178,
-  enfants: 157,
+  travail: 553,
+  achats_culturel_cultuel: 482,
+  sante: 434,
+  famille: 410,
+  handicap: 373,
+  sport_animaux: 349,
+  convocation: 276,
+  missions: 252,
+  enfants: 228,
 }
 
 export async function generatePdf (profile, reasons, pdfBase) {
@@ -70,15 +70,15 @@ export async function generatePdf (profile, reasons, pdfBase) {
     page1.drawText(text, { x, y, size, font })
   }
 
-  drawText(`${firstname} ${lastname}`, 107, 657)
-  drawText(birthday, 107, 627)
-  drawText(placeofbirth, 240, 627)
-  drawText(`${address} ${zipcode} ${city}`, 124, 596)
+  drawText(`${firstname} ${lastname}`, 92, 702)
+  drawText(birthday, 92, 684)
+  drawText(placeofbirth, 214, 684)
+  drawText(`${address} ${zipcode} ${city}`, 104, 665)
 
   reasons
     .split(', ')
     .forEach(reason => {
-      drawText('x', 59, ys[reason], 12)
+      drawText('x', 47, ys[reason], 12)
     })
 
   let locationSize = getIdealFontSize(font, profile.city, 83, 7, 11)
@@ -91,9 +91,9 @@ export async function generatePdf (profile, reasons, pdfBase) {
     locationSize = 7
   }
 
-  drawText(profile.city, 93, 122, locationSize)
-  drawText(`${profile.datesortie}`, 76, 92, 11)
-  drawText(`${profile.heuresortie}`, 246, 92, 11)
+  drawText(profile.city, 78, 76, locationSize)
+  drawText(`${profile.datesortie}`, 63, 58, 11)
+  drawText(`${profile.heuresortie}`, 227, 58, 11)
 
   // const shortCreationDate = `${creationDate.split('/')[0]}/${
   //   creationDate.split('/')[1]
@@ -111,7 +111,7 @@ export async function generatePdf (profile, reasons, pdfBase) {
 
   const qrImage = await pdfDoc.embedPng(generatedQR)
 
-  page1.drawText(qrTitle1 + '\n' + qrTitle2, { x: 415, y: 135, size: 9, font, lineHeight: 10, color: rgb(1, 1, 1) })
+  page1.drawText(qrTitle1 + '\n' + qrTitle2, { x: 440, y: 130, size: 6, font, lineHeight: 10, color: rgb(1, 1, 1) })
 
   page1.drawImage(qrImage, {
     x: page1.getWidth() - 156,
